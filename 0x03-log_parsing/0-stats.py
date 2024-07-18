de8e1b2b841f3fc6663d2f8d421286ddcccf486a
Benjamin <benjminrichie@gmail.com>
2024-07-18 21:19:33 +0000

    new

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -9,7 +9,7 @@ import re
 
 def output(log: dict) -> None:
     """
-    helper function to display stats
+    Helper function to display stats
     """
     print("File size: {}".format(log["file_size"]))
     for code in sorted(log["code_frequency"]):
@@ -19,22 +19,21 @@ def output(log: dict) -> None:
 
 if __name__ == "__main__":
     regex = re.compile(
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - 
-    \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] 
-    "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - '
+        r'\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] '
+        r'"GET /projects/260 HTTP/1.1" (.{3}) (\d+)'
+    )
 
     line_count = 0
     log = {}
     log["file_size"] = 0
-    log["code_frequency"] = {
-        str(code): 0 for code in [
-            200, 301, 400, 401, 403, 404, 405, 500]}
+    log["code_frequency"] = {str(code): 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]}
 
     try:
         for line in sys.stdin:
             line = line.strip()
             match = regex.fullmatch(line)
-            if (match):
+            if match:
                 line_count += 1
                 code = match.group(1)
                 file_size = int(match.group(2))
@@ -42,11 +41,12 @@ if __name__ == "__main__":
                 # File size
                 log["file_size"] += file_size
 
-                # status code
-                if (code.isdecimal()):
+                # Status code
+                if code.isdecimal():
                     log["code_frequency"][code] += 1
 
-                if (line_count % 10 == 0):
+                if line_count % 10 == 0:
                     output(log)
     finally:
         output(log)
+
